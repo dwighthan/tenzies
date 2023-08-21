5f7a1185b8cc640286d3ebcfe5e886fd6ae22440
Dwight Han <dwight1998@gmail.com>
2023-08-21 16:36:53 -0400

    project setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { React } from 'react';
+import './styles.css';
+
+function App() {
+  return (
+    <main className="main">
+      <div>hi</div>
+    </main>
+  );
+}
+
+export default App;
