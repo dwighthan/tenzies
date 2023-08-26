09376b5b77667d0421b893b7278fb04b43842d26
Dwight Han <dwight1998@gmail.com>
2023-08-25 22:04:42 -0400

    major functionality changes. big progress on project. adding details now

--- src/App.js
+++ src/App.js
@@ -2,13 +2,30 @@ import React from 'react';
 import Die from './components/Die';
 import './styles.css';
 import { nanoid } from 'nanoid'
+import Confetti from 'react-confetti';
 
 function App() {
 
-    //our state for dice values
+    //our state for dice objects. This array of objects will hold information we can use to build our dice components. 
     const [dice, setDice] = React.useState(randomArray())
 
-    //function to give us an array of objects to set to state. Gives us random number. 
+    //our state that changes when the game ends. 
+    const [tenzies, setTenzies] = React.useState(false)
+
+
+    //useEffect in order to keep our two states in sync 
+    React.useEffect(() => {
+      const firstValue = dice[0].value 
+      const isAllHeld = dice.every((die) => (die.isHeld === true))
+      const isAllSame = dice.every((die) => (die.value === firstValue))
+      if (isAllHeld && isAllSame) {
+        setTenzies(true)
+      }
+    }
+    
+    , [dice])
+
+    //function to give us an array of objects to set to state. Uses for loop to create 10 objects. 
     function randomArray() {
       const diceValueArray = []
       for (let i = 0; i < 10; i++) {
@@ -22,7 +39,7 @@ function App() {
      return diceValueArray
     }
     
-    //function for button to reset dice
+    //function for button to reset dice values
     function newDice() {
       setDice((prevDice) => {
         return prevDice.map((die) => {
@@ -31,6 +48,7 @@ function App() {
       })
     }
 
+    //function to click on dice. clicking dice will change the isHeld value. 
     function holdDice(id) {
       setDice((prevDice) => {
         return prevDice.map((die) => {
@@ -41,16 +59,37 @@ function App() {
       })  
     }
 
-    //mapping over state to turn into JSX elements
+    //function to reset the game 
+    function newGame() {
+      setTenzies(false)
+      setDice((prevDice) => {
+        return (
+          prevDice.map((die) => (
+          {
+           value: Math.ceil(Math.random() * 6),
+           isHeld: false,
+           id: nanoid()
+          }
+          ))
+        )
+      })
+    }
+
+    //mapping over state to turn our array of objects into JSX elements
     const diceArray = dice.map((die) => (<Die key={die.id} value={die.value} isHeld={die.isHeld} holdDice={() => holdDice(die.id)}/>))
 
   return (
     <main className="main">
       <h1 className="header">Tenzies</h1>
       <h3 className="header-instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</h3>
+      {tenzies && <Confetti></Confetti>}
       <div className="die-container">
         {diceArray}
+        {
+        tenzies ? 
+        <button className="button-reroll" onClick={newGame}>New Game</button> : 
         <button className="button-reroll" onClick={newDice}>Roll Again</button>
+        }
       </div>
     </main>
   );
--- src/components/Die.js
+++ src/components/Die.js
@@ -3,13 +3,102 @@ import "../styles.css"
 
 export default function Die(props) {
     
+    //color change when die is clicked
     const styles = {
         backgroundColor: props.isHeld ? "#59E391" : "#FFF"
     }
 
+    //didnt know how to create conditional die faces based on die number value in CSS. 
+    //Decided to write the code using Conditional Styling in my component.
+
+    //declaration of CSS Grid Variables
+    let dot
+    let dotTypeA
+    let dotTypeB
+    let dotTypeC
+    let dotTypeD
+    let dotTypeE
+    let dotTypeF
+    let dotTypeG
+
+    //conditional to style dice based on value
+    if (props.value === 1) {
+        return (
+                dotTypeD = {gridArea: "d"},
+
+                dot = <div className="die-face" style={styles} onClick={props.holdDice}>
+                        <span style={dotTypeD} className="dot"></span>
+                      </div>
+                )
+    } else if (props.value === 2) {
+        return (
+                dotTypeA = {gridArea: "a"}, dotTypeG = {gridArea: "g"},
+
+                dot = <div className="die-face" style={styles} onClick={props.holdDice}>
+                        <span style={dotTypeA} className="dot"></span>
+                        <span style={dotTypeG} className="dot"></span>
+                      </div>
+                )
+    } else if (props.value === 3) {
+        return (
+                dotTypeA = {gridArea: "a"}, dotTypeD = {gridArea: "d"}, dotTypeG = {gridArea: "g"}, 
+
+                dot = <div className="die-face" style={styles} onClick={props.holdDice}>
+                         <span style={dotTypeA} className="dot"></span>
+                         <span style={dotTypeD} className="dot"></span>
+                         <span style={dotTypeG} className="dot"></span>
+                      </div>
+                )
+    } else if (props.value === 4) {
+        return (
+                dotTypeA = {gridArea: "a"}, dotTypeB = {gridArea: "b"},  dotTypeF = {gridArea: "f"},  dotTypeG = {gridArea: "g"},
+
+                dot = <div className="die-face" style={styles} onClick={props.holdDice}>
+                        <span style={dotTypeA} className="dot"></span>
+                        <span style={dotTypeB} className="dot"></span>
+                        <span style={dotTypeF} className="dot"></span>
+                        <span style={dotTypeG} className="dot"></span>
+                      </div>
+                )
+    } else if (props.value === 5) {
+        return (
+                dotTypeA = {gridArea: "a"}, dotTypeB = {gridArea: "b"},  dotTypeD = {gridArea: "d"}, dotTypeF = {gridArea: "f"},
+                dotTypeG = {gridArea: "g"},
+                
+
+                dot = <div className="die-face" style={styles} onClick={props.holdDice}>
+                        <span style={dotTypeA} className="dot"></span>
+                        <span style={dotTypeB} className="dot"></span>
+                        <span style={dotTypeD} className="dot"></span>
+                        <span style={dotTypeF} className="dot"></span>
+                        <span style={dotTypeG} className="dot"></span>
+                      </div>
+                )
+    } else if (props.value === 6) {
+        return (
+                dotTypeA = {gridArea: "a"}, dotTypeB = {gridArea: "b"},  dotTypeC = {gridArea: "c"},  dotTypeE = {gridArea: "e"},
+                dotTypeF = {gridArea: "f"}, dotTypeG = {gridArea: "g"},
+
+                dot = <div className="die-face" style={styles} onClick={props.holdDice}>
+                        <span style={dotTypeA} className="dot"></span>
+                        <span style={dotTypeB} className="dot"></span>
+                        <span style={dotTypeC} className="dot"></span>
+                        <span style={dotTypeE} className="dot"></span>
+                        <span style={dotTypeF} className="dot"></span>
+                        <span style={dotTypeG} className="dot"></span>
+                      </div>
+                )
+    }
+
+
+    //render dot variable which will carry our JSX element
     return (
-        <div className="die-face" style={styles} onClick={props.holdDice}>
-            <h2 className="die-number">{props.value}</h2>
-        </div>
+        {dot}
     )    
 }
+
+// return (
+//     <div className="die-face" style={styles} onClick={props.holdDice}>
+//         <h2 className="die-number">{props.value}</h2>
+//     </div>
+// )    
