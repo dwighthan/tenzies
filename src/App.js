cf01e1b643936fa618aa9fb816778d4782048026
Dwight Han <dwight1998@gmail.com>
2023-08-30 00:00:03 -0400

    first attempt at timer

--- src/App.js
+++ src/App.js
@@ -13,27 +13,65 @@ function App() {
     //our state that changes when the game ends. 
     const [tenzies, setTenzies] = React.useState(false)
 
-    //state to start and stop timer
-    const [timer, setTimer] = React.useState(false)
+    //state to display timer
+    const [displayTimer, setDisplayTimer] = React.useState(false)
+
+    //issues
+    // clearInterval does not seem to be stopping my timer
+    // figure out which button will do what. Do we need another button to show the time?
+  
+  //TIMER CODE   
+
+    //two states to hold time when game starts and when game finishes
+    const [time, setTime] = React.useState()
+    const [now, setNow] = React.useState()
+ 
+    //useRef will help us run a timer without rerendering our entire component
+    const intervalRef = React.useRef(null)
+
+    let stopTimer
 
     
 
+    function startGame() {
+      setTime(Date.now())
+      setNow(Date.now())
+      stopTimer = setInterval(() => (
+          setTime(Date.now())
+      ), 10);
+    } 
+
+    function endGame() {
+      clearInterval(stopTimer)
+      setTime((prevState) => (prevState))
+      setNow((prevState) => (prevState))
+    }
+      // 
+
+    //   if (isNotAllHeld) {
+    //     setDisplayTimer(false)
+    //   } else {
+    //     setDisplayTimer(true)
+    //     setTime(Date.now())
+    //     setNow(Date.now())
+    //     intervalRef.current = setInterval(() => {
+    //     setTime(Date.now())
+    //       }, 10)
+    //   }
+    // }
+
+  //END OF TIMER CODE      
+    
     //useEffect in order to keep our two states in sync 
     React.useEffect(() => {
       const firstValue = dice[0].value 
-      const isNotAllHeld = dice.every((die) => (die.isHeld === false))
       const isAllHeld = dice.every((die) => (die.isHeld === true))
       const isAllSame = dice.every((die) => (die.value === firstValue))
       if (isAllHeld && isAllSame) {
-        setTenzies(true)
-      } else if (isNotAllHeld) {
-        setTimer(false)
-      } else if (isNotAllHeld !== true) {
-        setTimer(true)
-      }
-    } 
-    
-    , [dice])
+          setTenzies(true)
+        } 
+      } 
+      , [dice])
 
     //function to give us an array of objects to set to state. Uses for loop to create 10 objects. 
     function randomArray() {
@@ -83,40 +121,33 @@ function App() {
           ))
         )
       })
+
+      setDisplayTimer((prevState) => (!prevState))
     }
-
-    //function to time the game
-    function timerStart() {
-
-    }
-
-    function button() {
-      if (timer) {
-        return (<button className="button-reroll" onClick={newGame}>Roll Again</button>)
-      } else if (tenzies) {
-        return (<button className="button-reroll" onClick={newGame}>New Game</button>)
-      } else {
-        return (<button className="button-reroll" onClick={newDice}>Roll Again</button>)
-      }
-    } 
 
     //mapping over state to turn our array of objects into JSX elements
     const diceArray = dice.map((die) => (<Die key={die.id} value={die.value} isHeld={die.isHeld} holdDice={() => holdDice(die.id)}/>))
 
+    //timeValue made to string to assign to JSX element
+    let timePassed = (time - now) / 1000;
+    
   return (
     <div>
     <main className="main">
-      <h1 className="header">Tenzies</h1>
+    <h1 className="header">Tenzies</h1>
       <h3 className="header-instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</h3>
       {tenzies && <Confetti></Confetti>}
       <div className="die-container">
         {diceArray}
-        {button()}
       </div>
-      {
-      timer ? <Timer date={Date.now}/>
-      : <h3 className="header-instructions-2">The game will start when you click your first die.</h3> 
-      }
+      <div className="button-container">
+          <button className="button-reroll" onClick={newDice}>Roll Again</button>
+          <button className="button-reroll" onClick={endGame}> Start Game </button>
+          <button className="button-reroll" onClick={startGame}>{timePassed}</button>
+      </div>
+   
+      {/* <h3 className="header-instructions-2">The game will start when you click your first die.</h3>  */}
+      
     </main>
     </div>
   );
